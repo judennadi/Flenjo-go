9934a9fc333a635e16fd0154acec0c8290bf8964
Jude Nnadi <judeonnadi@gmail.com>
2023-02-27 01:36:20 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,11 @@
 # store the terraform state file in s3 and lock with dynamodb
 
-terraform {
-  backend "s3" {
-    bucket         = "hazard124"
-    key            = "flenjo/terraform.tfstate"
-    region         = "us-east-1"
-    profile        = "default"
-    dynamodb_table = "terraform-state-lock"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "hazard124"
+#     key            = "flenjo/terraform.tfstate"
+#     region         = "us-east-1"
+#     profile        = "default"
+#     dynamodb_table = "terraform-state-lock"
+#   }
+# }
